4db11c845a6ad596ae603b7dd17f79e4293bd7db
kaeytee <austinbediako4@gmail.com>
2025-10-17 07:45:11 +0000

    refactor: remove unused FiSend and FiMapPin icon imports from WaybillViewer

--- src/components/warehouse/WaybillViewer.tsx
+++ src/components/warehouse/WaybillViewer.tsx
@@ -9,7 +9,7 @@
  */
 
 import React, { useState, useEffect, useRef } from 'react';
-import { FiPrinter, FiDownload, FiX, FiPackage, FiSend, FiMapPin } from 'react-icons/fi';
+import { FiPrinter, FiDownload, FiX, FiPackage } from 'react-icons/fi';
 import { warehouseDocumentService } from '../../services/warehouseDocumentService';
 import type { WaybillData } from '../../services/warehouseDocumentService';
 import { useWarehouseAuth } from '../../hooks/useWarehouseAuth';
